99ec678fba6a8fc9caaa77b6d6ee41d25b50444b
ljfeyrer <ljfeyrer@dal.ca>
2025-04-27 15:26:11 -0700

    Update Melons.R

--- scripts/Melons.R
+++ scripts/Melons.R
@@ -55,12 +55,8 @@ clean_catalogue <- function(file_path) {
     # Assign sex safely to preserve UNKs where no sex is
     mutate(Sex = case_when(
       str_detect(Keywords, "\\bFemale-Juvenile\\b") ~ "FemaleJ",
-      str_detect(Keywords, "\\bFemaleJ\\b") ~ "FemaleJ",
-      str_detect(Keywords, "\\bSex_F\\b") ~ "FemaleJ",
-      str_detect(Keywords, "\\bF\\b") ~ "FemaleJ",
-      str_detect(Keywords, "\\bMale\\b") ~ "MaleM",
-      str_detect(Keywords, "\\bSex_M\\b") ~ "MaleM",
-      str_detect(Keywords, "\\bM\\b") ~ "MaleM",
+      str_detect(Keywords, "\\bSum_F\\b") ~ "FemaleJ",
+      str_detect(Keywords, "\\bSum_M\\b") ~ "MaleM",
       str_detect(Keywords, "\\bMM\\b") ~ "MaleM",
       str_detect(Keywords, "\\bFJ\\b") ~ "FemaleJ",
       TRUE ~ "UNK"
@@ -74,7 +70,48 @@ clean_catalogue <- function(file_path) {
 annual_mel <- clean_catalogue(here("INPUT/catalogue_files/LV_melons_2024.csv"))
 primary_mel <- clean_catalogue(here("INPUT/catalogue_files/LV_melons_primary.csv"))
 
-# CHECK: Clean UNK where known Sex exists for the same ID
+
+# Function to assess when there are multiple distinct Sex classes ("FemaleJ" vs. "MaleM") exist per ID.
+# # If there's more than one distinct Sex for an ID, it flags it.
+
+check_conflicting_sex <- function(df) {
+  conflicting_ids <- df %>%
+    filter(Sex %in% c("MaleM", "FemaleJ")) %>%
+    group_by(ID) %>%
+    summarise(unique_sexes = n_distinct(Sex)) %>%
+    filter(unique_sexes > 1) %>%
+    pull(ID)
+  
+     return(conflicting_ids)
+}
+
+      #check conflicting sex records----
+      
+      conflicting_sex_ids = check_conflicting_sex(primary_mel)
+      
+      check = primary_mel %>%
+        filter(ID %in% conflicting_sex_ids) %>%
+        select(ID, Date, Sex) %>%
+        arrange(ID, Date)
+      
+      ## Clean UNK or FJ where MM Sex exists for the same ID
+      primary_mel <- primary_mel %>%
+        group_by(ID) %>%
+        mutate(
+          # 1. Set winning sex for each ID
+          final_sex = case_when(
+            "MaleM" %in% Sex ~ "MaleM",      # MaleM wins
+            "FemaleJ" %in% Sex ~ "FemaleJ",  # Otherwise FemaleJ
+            TRUE ~ "UNK"                    # Otherwise UNK
+          ),
+          # 2. Overwrite ALL rows with the final_sex
+          Sex = final_sex
+        ) %>%
+        select(-final_sex) %>%
+        ungroup()
+      
+###
+
 annual_mel <- annual_mel %>%
   group_by(ID) %>%
   mutate(Sex = ifelse(
@@ -117,3 +154,5 @@ sex_mismatch <- sex_comparison %>%
 
 # Step 4: View
 sex_mismatch
+
+write_csv(sex_mismatch, "OUTPUT/sex_mismatch_2024.csv")
